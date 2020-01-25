ddf11509ce5384e2c9ba1a8d4ab1ba62b3b0834a
CorneliusCornbread <22rosenj@elmbrookstudents.org>
2020-01-25 15:54:15 -0600

    Created subsystem class
    Just some initial commit stuff

---
+++ FRC-2020/src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -0,0 +1,21 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+/**
+ * Subsystem that manages the climbing arm.
+ * Manages lifting of robot and the folding of the arm.
+ */
+public class ClimberSubsystem extends SubsystemBase
+{
+    
+
+    //Default constructor
+    public ClimberSubsystem() { }
+
+    @Override
+    public void periodic() 
+    {
+
+    }
+}

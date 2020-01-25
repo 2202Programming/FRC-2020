ea30fe3475561b52d5760d076cbc13902e9dd90c
Unknown <22rubens@elmbrookstudents.org>
2020-01-25 10:24:24 -0600

    Angle

--- FRC-2020/src/main/java/frc/robot/subsystems/Lidar_Subsystem.java
+++ FRC-2020/src/main/java/frc/robot/subsystems/Lidar_Subsystem.java
@@ -23,6 +23,9 @@ public class Lidar_Subsystem extends SubsystemBase {
   private double left_lidar_range;
   private double right_lidar_range;
   private long lastLidarTime;
+  private final double LIDAR_DIST = 368.3;
+  private double angle;
+  private boolean validRange;
 
   public Lidar_Subsystem() {
 
@@ -44,12 +47,34 @@ public class Lidar_Subsystem extends SubsystemBase {
       left_lidar_range = front_left_lidar.getRange();
       right_lidar_range = front_right_lidar.getRange();
       lastLidarTime = System.currentTimeMillis();
+      findAngle();
+      validRange = valid();
     }
+  }
+
+  public void findAngle(){
+    //getRange() returns distance in milimeters 
+    double dist2 = left_lidar_range;
+    double dist1 = right_lidar_range;
+  
+    double difference = dist1 - dist2;
+  
+    angle = Math.toDegrees(Math.atan(difference/LIDAR_DIST));
+
   }
 
   public void printLog() {
     SmartDashboard.putNumber("Front Left Lidar", left_lidar_range);
     SmartDashboard.putNumber("Front Right Lidar", right_lidar_range);
+    SmartDashboard.putNumber("Lidar Angle", angle);
+    SmartDashboard.putBoolean("Range is valid", validRange);
+  }
+
+  public boolean valid(){
+    if(front_left_lidar.isRangeValid() == false || front_right_lidar.isRangeValid() == false){
+      return false;
+    }
+    return true;
   }
 
   @Override

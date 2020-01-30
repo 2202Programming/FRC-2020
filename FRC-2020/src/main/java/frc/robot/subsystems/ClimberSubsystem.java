67513c3309149186b76d6dcb8a15fd8469b3c41a
Unknown <michael.lin2032@gmail.com>
2020-01-29 19:15:04 -0600

    Initial climbSubsystem knowledge to be built

--- FRC-2020/src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ FRC-2020/src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -8,10 +8,18 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
  */
 public class ClimberSubsystem extends SubsystemBase
 {
-    
+    /** Initial knowledge of how the subsystem is to be run
+     * 775 motor used for rotation(Talon controller)
+     * neo motor used to shoot arm up(sparkmax controller)
+     * mechanical stop for rotation
+     * Ask Kevin for more info from Kevin
+     */
 
     //Default constructor
-    public ClimberSubsystem() { }
+    public ClimberSubsystem() 
+    { 
+
+    }
 
     @Override
     public void periodic() 

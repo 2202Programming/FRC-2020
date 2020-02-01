3acb675392729e2882e952486f6695880407b8a1
Unknown <21fengs@elmbrookstudents.org>
2020-02-01 11:29:30 -0600

    Added more Util commands

---
+++ FRC-2020/src/main/java/frc/robot/util/CallFunctionCommand.java
@@ -0,0 +1,19 @@
+package frc.robot.util;
+
+import java.util.function.IntSupplier;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+
+public class CallFunctionCommand extends InstantCommand {
+  IntSupplier workFunct;
+
+  public CallFunctionCommand(IntSupplier workFunct) {
+    this.workFunct = workFunct;
+  }
+
+    // Called repeatedly when this Command is scheduled to run
+    @Override
+    public void execute() {
+        workFunct.getAsInt();
+    }
+
+}

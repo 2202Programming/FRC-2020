085afec8cdcfde184f6695b085378cc176800dd1
Unknown <22rubens@elmbrookstudents.org>
2020-02-01 09:44:12 -0600

    limelight updates and logs

--- FRC-2020/src/main/java/frc/robot/subsystems/Limelight_Subsystem.java
+++ FRC-2020/src/main/java/frc/robot/subsystems/Limelight_Subsystem.java
@@ -8,17 +8,75 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import frc.robot.Robot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Limelight_Subsystem extends SubsystemBase {
   /**
    * Creates a new Limelight_Subsystem.
    */
-  public Limelight_Subsystem() {
 
+   private NetworkTable table;
+   private double x;
+   private double y;
+   private double area; //area is between 0 and 100. Calculated as a percentage of image
+   private double target;
+   private long logTimer;
+   
+  public Limelight_Subsystem() {
+    logTimer = System.currentTimeMillis();
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    table = NetworkTableInstance.getDefault().getTable("limelight");
+    NetworkTableEntry tx = table.getEntry("tx"); //-27 degrees to 27 degrees
+    NetworkTableEntry ty = table.getEntry("ty"); // -20.5 to 20.5 degrees
+    NetworkTableEntry ta = table.getEntry("ta");
+    NetworkTableEntry tv = table.getEntry("tv"); //target validity (1 or 0)
+
+    //updates global variables
+
+    x = tx.getDouble(0.0);
+    y = ty.getDouble(0.0);
+    area = ta.getDouble(0.0);
+    target = tv.getDouble(0.0);
+    
+    log(10);
+  }
+
+  public double getX(){
+    return x;
+  }
+
+  public double getY(){
+    return y;
+  }
+
+  public double getArea(){
+    return area;
+  }
+
+  public double getTarget(){
+    return target;
+  }
+
+  public void log(int interval){
+    if((interval + logTimer) < System.currentTimeMillis()){ //interval in ms
+      logTimer = System.currentTimeMillis();
+
+      SmartDashboard.putNumber("X value", x);
+      SmartDashboard.putNumber("Y value", y);
+      SmartDashboard.putNumber("Area", area);
+      SmartDashboard.putNumber("target", target);
+
+    }
   }
 }

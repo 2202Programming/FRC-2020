99cbb303a6b78490a6871ecafec0727fd848e1f2
CorneliusCornbread <22rosenj@elmbrookstudents.org>
2020-02-01 13:08:26 -0600

    Skeleton of subsystem
    WIP

--- FRC-2020/src/main/java/frc/robot/Constants.java
+++ FRC-2020/src/main/java/frc/robot/Constants.java
@@ -32,5 +32,6 @@ public final class Constants {
     public static final int E_SPARKMAX_CANID = 14; //Extension Motor
     public static final int E_ROT_SPARKMAX_CANID = 15; //Extension Arm Rotation Motor
     public static final int WN_SPARKMAX_CANID = 16; //Winch Motor
+    public static final int FLOOR_SENSOR = 17; //Time of Flight sensor, measures distance from floor
     //endregion
 }
--- FRC-2020/src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ FRC-2020/src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -1,6 +1,11 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 /**
  * Subsystem that manages the climbing arm.
@@ -8,14 +13,58 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
  */
 public class ClimberSubsystem extends SubsystemBase
 {
-    
+    //region Motors
+    private CANSparkMax eSparkMax = new CANSparkMax(Constants.E_SPARKMAX_CANID, MotorType.kBrushed);  //Extension motor
+    private CANSparkMax eROTSparkMax = new CANSparkMax(Constants.E_ROT_SPARKMAX_CANID, MotorType.kBrushless); //Extension arm rotation motor
+    private CANSparkMax wnSparkMax = new CANSparkMax(Constants.WN_SPARKMAX_CANID, MotorType.kBrushless); //Winch motor
+    //endregion
+
+    private ArmState state = ArmState.FOLDED;
 
     //Default constructor
-    public ClimberSubsystem() { }
+    public ClimberSubsystem() 
+    {
+        //Set the motors to brake for testing purposes, feel free to change if needed
+        eSparkMax.setIdleMode(IdleMode.kBrake);
+        eROTSparkMax.setIdleMode(IdleMode.kBrake);
+        wnSparkMax.setIdleMode(IdleMode.kBrake);
+    }
 
     @Override
     public void periodic() 
     {
+        
+    }
 
+    /**
+     * Fold the robot's arm
+     */
+    public void foldArm()
+    {
+        
+    }
+
+    /**
+     * Unfold's the arm from the robot
+     */
+    public void unfoldArm()
+    {
+
+    }
+
+    /**
+     * Extend upper arm to begin grabbing
+     */
+    public void extendArm()
+    {
+        
+    }
+
+    public enum ArmState
+    {
+        FOLDED,
+        UNFOLDED,
+        EXTENDED,
+        GRABBED
     }
 }

2caa0bb504d3ace3c328701c7e21e477439ce26e
Xander Ge <xander.q.ge@gmail.com>
2020-02-03 20:43:11 -0600

    WIP Ramsete

--- FRC-2020/src/main/java/frc/robot/commands/drive/CurveDrive.java
+++ FRC-2020/src/main/java/frc/robot/commands/drive/CurveDrive.java
@@ -8,10 +8,17 @@
 package frc.robot.commands.drive;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
+import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.geometry.Translation2d;
+import edu.wpi.first.wpilibj.trajectory.Trajectory;
 import edu.wpi.first.wpilibj.trajectory.TrajectoryConfig;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryGenerator;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RamseteCommand;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.robot.RobotContainer;
 import frc.robot.subsystems.DriveTrain;
@@ -33,6 +40,13 @@ public class CurveDrive implements Command {
   @Override
   public void execute() {
     TrajectoryConfig trajectoryConfig = new TrajectoryConfig(DriveTrain.maxSpeed, DriveTrain.maxAccel);
+
+    Trajectory tempTrajectory = TrajectoryGenerator.generateTrajectory(
+        // Start @ (0, 0) facing +X
+        new Pose2d(0, 0, new Rotation2d(0)), List.of(new Translation2d(1, 1), new Translation2d(2, 1)),
+        new Pose2d(3, 1, new Rotation2d(0)), trajectoryConfig);
+    RamseteCommand ramsete = new RamseteCommand(tempTrajectory, pose, controller, feedforward, kinematics, wheelSpeeds, leftController, rightController, outputVolts, requirements);
+
   }
 
   // Called once the command ends or is interrupted.

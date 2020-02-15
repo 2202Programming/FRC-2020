75769b59cedf70e8353572d90b84bd99922ceb3f
AMOORCHING <akash.moorching@gmail.com>
2020-02-15 10:54:15 -0600

    Actually Instantiate Climber

--- FRC-2020/src/main/java/frc/robot/RobotContainer.java
+++ FRC-2020/src/main/java/frc/robot/RobotContainer.java
@@ -22,6 +22,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  private final ClimberSubsystem climber = new ClimberSubsystem();
 
   private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
 

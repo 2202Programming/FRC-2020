f23a4dd9813bbcdc5e757ad2bc816e64cb57f59f
CorneliusCornbread <22rosenj@elmbrookstudents.org>
2020-02-17 20:48:17 -0600

    Started work on autoclimb command
    Autoclimb currently doesn't do anything. PLEASE DO NOT SCHEDULE IT

--- FRC-2020/src/main/java/frc/robot/commands/AutonomousClimbCommand.java
+++ FRC-2020/src/main/java/frc/robot/commands/AutonomousClimbCommand.java
@@ -35,35 +35,23 @@ public class AutonomousClimbCommand extends CommandBase
         addRequirements(_climber);
     }
 
-    /**
-     * Checks the motor time and if the time is reached it stops them
-     */
-    private void checkMotorStop()
+    @Override
+    public void initialize() 
     {
-        if (rotState == RotState.UNFOLDING && ROT_DUR_TIME <= (System.currentTimeMillis() - rotStartTime))
-        {
-            rotState = RotState.UNFOLDED;
-            climber.setRotSpeed(0);
-        }
+        climber.stopRot();
+        climber.setWinchSpeed(0);
 
-        else if (rotState == RotState.FOLDING && ROT_DUR_TIME <= (System.currentTimeMillis() - rotStartTime))
-        {
-            rotState = RotState.FOLDED;
-            climber.setRotSpeed(0);
-        }
 
-        if (wnState == WinchState.EXTENDING && WN_DUR_TIME <= (System.currentTimeMillis() - wnStartTime))
-        {
-            wnState = WinchState.EXTENDED;
-            climber.setWinchSpeed(0);
+    }
 
-        }
+    public void fold()
+    {
 
-        else if (wnState == WinchState.RETRACTING && WN_DUR_TIME <= (System.currentTimeMillis() - wnStartTime))
-        {
-            wnState = WinchState.RETRACTED;
-            climber.setWinchSpeed(0);
-        }
+    }
+
+    public void climb()
+    {
+        
     }
 
     public enum TargetState

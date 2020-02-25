e757e665248a9c63da255d11a511c9ccaa12f44e
Derek <derek@adminnet.us>
2020-02-25 13:30:05 -0600

    updated libs, added TalonSRX CTRE

--- FRC-2020/src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ FRC-2020/src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -5,10 +5,14 @@ import edu.wpi.first.wpilibj.Servo;
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
+import frc.robot.Constants.*;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.ControlType;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
 
 /**
  * Subsystem that manages the climbing arm motors.
@@ -16,21 +20,40 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 public class ClimberSubsystem extends SubsystemBase
 {
     //region
-    private DoubleSolenoid armSolenoid = new DoubleSolenoid(6, 7); //Arm valves
+    private DoubleSolenoid armSolenoid = new DoubleSolenoid(6, 7); //Arm valves TODO:put in constants
     //endregion
 
     //region Motors
-    private Servo eRotServo = new Servo(Constants.E_ROT_SERVO_PWM); //Arm rotation motor
-    private CANSparkMax wnSparkMax = new CANSparkMax(Constants.WN_SPARKMAX_CANID, MotorType.kBrushless); //Winch motor - extend / retract arm
+    private TalonSRX eRotServo = new TalonSRX(18); // Arm rotation motor TODO:put in constants
+    private ControlMode mode = ControlMode.Position;
+    private int countsPerRotation = 4096;
+    
+    private CANSparkMax wnSparkMax = new CANSparkMax(WN_SPARKMAX_CANID, MotorType.kBrushless); //Winch motor - extend / retract arm
     //endregion
 
     public ClimberSubsystem() 
     {
         armSolenoid.set(DoubleSolenoid.Value.kOff);
+        configureTalon(false, kp, ki, kd, kf);
+
         eRotServo.set(0);
         wnSparkMax.set(0);
         wnSparkMax.setIdleMode(IdleMode.kBrake);
     }
+
+    void configureTalon(double kp, double ki, double kd, double kf){
+		eRotServo.i(port,reverse);
+		mode = ControlMode.Position;
+		part.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, 0);
+		part.config_kP(0, kp, 0);
+		part.config_kI(0, ki, 0);
+		part.config_kD(0, kd, 0);
+		part.config_kF(0, kf, 0);
+		countsPerRotation = 4096;
+		part.setIntegralAccumulator(0.0,0,0);
+		part.setSelectedSensorPosition(0, 0, 0);
+	}
+
 
     /**
      * Takes in a number and clamps it so it's min is -1 and it's max is 1

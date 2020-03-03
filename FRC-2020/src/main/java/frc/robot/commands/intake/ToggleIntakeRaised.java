02ae5f4da9ea5034c78272e3d970e45b67e1c8c6
Derek <derek@adminnet.us>
2020-03-02 19:46:58 -0600

    Mag/Intake interlock for up/down requests

--- FRC-2020/src/main/java/frc/robot/commands/intake/ToggleIntakeRaised.java
+++ FRC-2020/src/main/java/frc/robot/commands/intake/ToggleIntakeRaised.java
@@ -11,7 +11,6 @@ import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.subsystems.Intake_Subsystem;
 
 public class ToggleIntakeRaised extends InstantCommand {
-  private boolean intakeUp = true;
 
   private Intake_Subsystem m_intake;
   public ToggleIntakeRaised(Intake_Subsystem m_intake) {
@@ -22,7 +21,7 @@ public class ToggleIntakeRaised extends InstantCommand {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-
+    boolean intakeUp = m_intake.isIntakeUp();
     if (intakeUp) {
       m_intake.lowerIntake();
       //off is intentional on lowering according to Kyle.
@@ -30,8 +29,5 @@ public class ToggleIntakeRaised extends InstantCommand {
       m_intake.raiseIntake();
       m_intake.intakeOff();
     }
-
-    //flip state
-    intakeUp = !intakeUp;
   }
 }

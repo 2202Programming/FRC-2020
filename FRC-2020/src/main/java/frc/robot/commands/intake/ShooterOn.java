053e32bc8aa24ef4d8982636fcf0b2423ca479ea
Sarah <22rubens@elmbrookstudents.org>
2020-12-02 17:12:04 -0600

    setting stage to 0 at end

--- FRC-2020/src/main/java/frc/robot/commands/intake/ShooterOn.java
+++ FRC-2020/src/main/java/frc/robot/commands/intake/ShooterOn.java
@@ -105,6 +105,7 @@ public class ShooterOn extends CommandBase {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    stage = 0;
     m_intake.shooterOff();
     m_intake.magazineOff();
     m_intake.intakeOff();

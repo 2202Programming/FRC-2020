bf9f08a810388e5afff7b532533b1e169f32f2b3
Unknown <21fengs@elmbrookstudents.org>
2020-12-02 20:42:24 -0600

    Testing Parser

--- FRC-2020/src/main/java/frc/robot/xmlParser.java
+++ FRC-2020/src/main/java/frc/robot/xmlParser.java
@@ -13,12 +13,14 @@ public class xmlParser {
             DocumentBuilder builder = factory.newDocumentBuilder();
             Document doc = builder.parse(robotFile);
             
-            NodeList paths = doc.getElementsByTagName("Paths");
+            NodeList paths = doc.getElementsByTagName("Path");
+            System.out.println(paths.getLength()); //currently prints 1
 
             Node selectedPath = paths.item(0);
-            System.out.println(selectedPath);
+            System.out.println(selectedPath); //currently prints [Path: null]
 
-            //System.out.println(selectedPath.item(0).getAttributes());
+            System.out.println(selectedPath.getAttributes().item(0)); //currently prints ID="HighMode"
+
 
 
         } catch (Exception e) {

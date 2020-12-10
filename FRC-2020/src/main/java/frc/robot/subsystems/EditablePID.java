033a96342a70198826f75146db208940fd340143
Kevin Li <21lik@elmbrookstudents.org>
2020-12-09 19:40:00 -0600

    Documentation for PIDEditor and EditablePID

--- FRC-2020/src/main/java/frc/robot/subsystems/EditablePID.java
+++ FRC-2020/src/main/java/frc/robot/subsystems/EditablePID.java
@@ -11,26 +11,60 @@ import frc.robot.util.pid.PIDEditor;
  * @author Kevin Li
  */
 public interface EditablePID extends Subsystem, Sendable {
+    /**
+     * The respective PIDEditor for this subsystem.
+     */
     PIDEditor pidEditor = new PIDEditor();
 
+    /**
+     * Initializes the subsystem's PIDEditor. Should be called once and only once, at the initialization
+     * or construction of the subsystem.
+     */
     default void initializePIDEditor() {
         pidEditor.initialize(this); // only to be run once per subsystem (should it be changed for different modes (auto, teleop, etc.)?)
     }
 
+    /**
+     * Gets the current P value of the subsystem.
+     * @return the current p
+     */
     double getP();
 
+    /**
+     * Gets the current I value of the subsystem.
+     * @return the current i
+     */
     double getI();
 
+    /**
+     * Gets the current D value of the subsystem.
+     * @return the current d
+     */
     double getD();
 
+    /**
+     * Gets the updated P value for the subsystem. The implementing subsystem class should periodically
+     * invoke this and set the P instance to it.
+     * @return the new p
+     */
     default double getNewP() {
         return pidEditor.updateP();
     }
 
+    /**
+     * Gets the updated I value for the subsystem. The implementing subsystem class should periodically
+     * invoke this and set the I instance to it.
+     * @return the new i
+     */
     default double getNewI() {
         return pidEditor.updateI();
     }
 
+    /**
+     * Gets the updated D value for the subsystem. The implementing subsystem class should periodically
+     * invoke this and set the D instance to it.
+     * @return the new d
+     */
     default double getNewD() {
         return pidEditor.updateD();
     }
--- FRC-2020/src/main/java/frc/robot/util/pid/PIDEditor.java
+++ FRC-2020/src/main/java/frc/robot/util/pid/PIDEditor.java
@@ -11,20 +11,37 @@ import frc.robot.subsystems.Log_Subsystem;
  * 
  * @author Kevin Li
  */
-public class PIDEditor { // as a separate object to be used by subsystems
+public class PIDEditor {
+    /**
+     * The subsystem using this object.
+     */
     private EditablePID subsystem;
+    /**
+     * The respective key for the subsystem, to use for the SmartDashboard.
+     */
     private String pKey, iKey, dKey;
+    /**
+     * Whether or not the PIDEditor was already initialized. The PIDEditor should only be initialized once.
+     */
     private boolean initialized = false;
 
+    /**
+     * Constructs a PIDEditor for the subsystem. Each subsystem with PID values that are intended to be
+     * editable and updatable dynamically should implement the EditablePID interface to construct and use
+     * one. The constructor is empty to allow the interface to construct one initially.
+     */
     public PIDEditor() {
-        // empty constructor to allow for construction by EditablePID interface
     }
 
+    /**
+     * Initializes the PIDEditor for the subsystem implementing the EditablePID interface. Can only be
+     * called once, and should be called once before any other methods on it are invoked.
+     * @param subsystem the subsystem with editable PID values. Should be set to <code>this</code>
+     */
     public void initialize(EditablePID subsystem) {
         if (this.initialized)
             throw new IllegalStateException("PIDEditor for subsystem " + SendableRegistry.getName(subsystem) + " was already initialized.");
 
-        // when called by a subsystem, pass <code>this</code> as the first implicit parameter
         this.subsystem = subsystem;
 
         this.pKey = SendableRegistry.getName(subsystem) + " P";
@@ -38,20 +55,60 @@ public class PIDEditor { // as a separate object to be used by subsystems
         SmartDashboard.putNumber(dKey, subsystem.getD());
     }
 
+    /**
+     * Gets the subsystem with which the PIDEditor is associated.
+     * @return the respective subsystem
+     */
     public EditablePID getSubsystem() {
         return subsystem;
     }
 
+    /**
+     * Gets the key used in the SmartDashboard to dynamically update the P value for this subsystem.
+     * @return the pKey
+     */
+    public String getPKey() {
+        return pKey;
+    }
+
+    /**
+     * Gets the key used in the SmartDashboard to dynamically update the I value for this subsystem.
+     * @return the iKey
+     */
+    public String getIKey() {
+        return iKey;
+    }
+
+    /**
+     * Gets the key used in the SmartDashboard to dynamically update the D value for this subsystem.
+     * @return the dKey
+     */
+    public String getDKey() {
+        return dKey;
+    }
+
+    /**
+     * Gets the new P value.
+     * @return the new p
+     */
     public double updateP() {
         double newP = SmartDashboard.getNumber(pKey, subsystem.getP());
         return newP;
     }
 
+    /**
+     * Gets the new I value.
+     * @return the new i
+     */
     public double updateI() {
         double newI = SmartDashboard.getNumber(iKey, subsystem.getI());
         return newI;
     }
 
+    /**
+     * Gets the new D value.
+     * @return the new d
+     */
     public double updateD() {
         double newD = SmartDashboard.getNumber(dKey, subsystem.getD());
         return newD;

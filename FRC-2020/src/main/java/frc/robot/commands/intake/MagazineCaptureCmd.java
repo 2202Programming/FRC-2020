5ca712f2bd0cf4020cb3072a9294703e267cfe59
DerekL <derek@adminnet.us>
2021-02-13 10:59:40 -0600

    Mag capture command

--- FRC-2020/src/main/java/frc/robot/commands/intake/MagazineCaptureCmd.java
+++ FRC-2020/src/main/java/frc/robot/commands/intake/MagazineCaptureCmd.java
@@ -8,26 +8,64 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Magazine_Subsystem;
 
 public class MagazineCaptureCmd extends CommandBase {
+
+  // constants
+  double kMotorStrength = 0.8;
+
+  Magazine_Subsystem mag;
+
+  // states of our command
+  enum State {
+    WaitingForPC, MovingPC, MagFull
+  };
+
+  State state = State.WaitingForPC;
+
   /** Creates a new MagazineCaptureCmd. */
   public MagazineCaptureCmd(Magazine_Subsystem mag) {
-    addRequirements(mag);  
+    this.mag = mag;
+    addRequirements(mag);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    state = State.WaitingForPC;
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+
+    switch (state) {
+      case WaitingForPC:
+
+        if (mag.isGateBlocked()) {
+          mag.beltOn(kMotorStrength);
+          state = State.MovingPC;
+        }
+        break;
+      case MovingPC:
+        if (!mag.isGateBlocked()) {
+          mag.beltOff();
+          mag.addPC();
+          state = (mag.isMagFull()) ? State.MagFull : State.WaitingForPC;
+        }
+        break;
+
+      case MagFull:
+      default:
+    }
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+  }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return (state == State.MagFull);
   }
 }

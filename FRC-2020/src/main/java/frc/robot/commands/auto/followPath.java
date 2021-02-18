a5343fb5199f6a68d69d1648a549ec3d14915427
Shawn <21fengs@elmbrookstudents.org>
2021-02-17 20:54:54 -0600

    Galactic Search Path Chooser
    Needs to be looked over

---
+++ FRC-2020/src/main/java/frc/robot/commands/auto/followPath.java
@@ -0,0 +1,73 @@
+package frc.robot.commands.auto;
+
+import edu.wpi.first.wpilibj.controller.RamseteController;
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveKinematics;
+import edu.wpi.first.wpilibj.trajectory.Trajectory;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.RamseteCommand;
+import frc.robot.subsystems.ifx.VelocityDrive;
+
+public class followPath extends CommandBase {
+  final VelocityDrive drive;
+
+  //
+  Trajectory trajectory;
+  RamseteCommand ramsete;
+  RamseteController rsController;
+  DifferentialDriveKinematics kinematics;
+
+  // Ramsete constants - todo wire to ux
+  double beta = 2.0;
+  double zeta = 0.7;
+
+  /** Creates a new followTrajectory. */
+  public followPath(VelocityDrive drive, Trajectory trajectory) {
+    this.drive = drive;
+    this.trajectory = trajectory;
+
+    kinematics = this.drive.getDriveKinematics();
+    addRequirements(drive);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    if (trajectory != null) {
+      // Reset odometry to the starting pose of the trajectory.
+      drive.resetOdometry(trajectory.getInitialPose());
+
+      // construct new Ramsete
+      rsController = new RamseteController(this.beta, this.zeta);
+      ramsete = new RamseteCommand(trajectory, drive::getPose, // odmetry package in drive
+          rsController, // outer loop non-linear controller (follower)
+          kinematics, // robot chassis model
+          drive::velocityTankDrive // vel output
+      /* no further requirements */);
+
+      // initize ramsete
+      ramsete.initialize();
+    }
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    if (ramsete != null)
+      ramsete.execute();
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    if (ramsete != null)
+      ramsete.end(interrupted);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    if (ramsete != null)
+      return ramsete.isFinished();
+    return true;
+  }
+}
---
+++ FRC-2020/src/main/java/frc/robot/commands/auto/galacticSearchA.java
@@ -0,0 +1,44 @@
+package frc.robot.commands.auto;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.trajectory.Trajectory;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryUtil;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.Magazine_Subsystem;
+import frc.robot.subsystems.VelocityDifferentialDrive_Subsystem;
+
+public class galacticSearchA extends SequentialCommandGroup {
+
+  public galacticSearchA(VelocityDifferentialDrive_Subsystem drive, Magazine_Subsystem magazine) {
+    String start = "paths/SearchAStart.wpilib.json";
+    
+    try {
+      Path startPath = Filesystem.getDeployDirectory().toPath().resolve(start);
+      Trajectory startTraj = TrajectoryUtil.fromPathweaverJson(startPath);
+      CommandBase cmd = new followPath(drive, startTraj);
+      addCommands(cmd);
+      while (!cmd.isFinished()) {}
+    } catch (IOException ex) {
+      DriverStation.reportError("Unable to open trajectory: " + start, ex.getStackTrace());
+    }
+
+    String choosen;
+    if (magazine.getPC() == 0)
+      choosen = "paths/SearchABlue.wpilib.json";
+    else
+      choosen = "paths/SearchARed.wpilib.json";
+    
+    try {
+      Path choosenPath = Filesystem.getDeployDirectory().toPath().resolve(choosen);
+      Trajectory choosenTraj = TrajectoryUtil.fromPathweaverJson(choosenPath);
+      addCommands (new followPath(drive, choosenTraj));
+    } catch (IOException ex) {
+      DriverStation.reportError("Unable to open trajectory: " + choosen, ex.getStackTrace());
+    }
+  }
+}

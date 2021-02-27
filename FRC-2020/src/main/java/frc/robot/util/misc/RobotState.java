1e522280446543eb13e34ba8595d293443e3ff2f
Jason <jruben4@yahoo.com>
2021-02-27 08:38:15 -0600

    robot state class

---
+++ FRC-2020/src/main/java/frc/robot/util/misc/RobotState.java
@@ -0,0 +1,41 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.util.misc;
+
+import edu.wpi.first.wpilibj.geometry.Pose2d;
+import frc.robot.subsystems.Intake_Subsystem;
+import frc.robot.subsystems.VelocityDifferentialDrive_Subsystem;
+
+/** Add your docs here. */
+public class RobotState {
+
+  private final VelocityDifferentialDrive_Subsystem driveTrain;
+  private final Intake_Subsystem intake;
+  private Pose2d savedPose;
+  private double savedMagAngle;
+
+  public RobotState(VelocityDifferentialDrive_Subsystem driveTrain, Intake_Subsystem intake){
+    this.driveTrain = driveTrain;
+    this.intake = intake;
+
+    savedPose = driveTrain.getPose();
+    savedMagAngle = intake.getMagazine().getAngle();
+    
+  }
+
+  public Pose2d getSavedPose(){
+    return savedPose;
+  }
+
+  public double getSavedMagAngle() 
+  {
+    return savedMagAngle;
+  }
+
+  public void saveRobotState(){
+    savedPose = driveTrain.getPose();
+    savedMagAngle = intake.getMagazine().getAngle();
+  }
+}

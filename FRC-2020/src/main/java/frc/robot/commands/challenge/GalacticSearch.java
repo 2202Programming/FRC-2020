4d9991910b2e10791359de6bdd6a79fad2e32019
Shawn <21fengs@elmbrookstudents.org>
2021-02-28 21:54:49 -0600

    GalacticSearch fix

--- FRC-2020/src/main/java/frc/robot/commands/challenge/GalacticSearch.java
+++ FRC-2020/src/main/java/frc/robot/commands/challenge/GalacticSearch.java
@@ -18,8 +18,18 @@ public class GalacticSearch extends SequentialCommandGroup {
   CommandBase endCmd;
   CommandBase startCmd;
 
-  //Creates a new GalacticSearch object
+  Trajectory startTraj;
+  Trajectory blueTraj;
+  Trajectory redTraj;
+
+  VelocityDifferentialDrive_Subsystem drive;
+  Magazine_Subsystem magazine;
+
+  // Creates a new GalacticSearch object
   public GalacticSearch(VelocityDifferentialDrive_Subsystem drive, Magazine_Subsystem magazine, boolean isA) {
+    this.drive = drive;
+    this.magazine = magazine;
+
     String start;
     String blue;
     String red;
@@ -35,40 +45,39 @@ public class GalacticSearch extends SequentialCommandGroup {
 
     try {
       Path startPath = Filesystem.getDeployDirectory().toPath().resolve(start);
-      Trajectory startTraj = TrajectoryUtil.fromPathweaverJson(startPath);
+      startTraj = TrajectoryUtil.fromPathweaverJson(startPath);
       Path bluePath = Filesystem.getDeployDirectory().toPath().resolve(blue);
-      Trajectory blueTraj = TrajectoryUtil.fromPathweaverJson(bluePath);
+      blueTraj = TrajectoryUtil.fromPathweaverJson(bluePath);
       Path redPath = Filesystem.getDeployDirectory().toPath().resolve(red);
-      Trajectory redTraj = TrajectoryUtil.fromPathweaverJson(redPath);
-
-      startCmd = new followTrajectory(drive, startTraj);
-      addCommands(startCmd);
-      while (!startCmd.isFinished()) { //waits until end of path before checking for a ball
-      }
-
-      Trajectory choosenTraj;
-      if (magazine.getPC() == 0)
-        choosenTraj = blueTraj;
-      else
-        choosenTraj = redTraj;
-      endCmd = new followTrajectory(drive, choosenTraj);
-      addCommands(endCmd);
+      redTraj = TrajectoryUtil.fromPathweaverJson(redPath);
     } catch (IOException ex) {
       DriverStation.reportError("Unable to open trajectory", ex.getStackTrace());
     }
   }
 
-  //Called when the command is initially scheduled.
+  // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    startCmd = new followTrajectory(drive, startTraj);
+    addCommands(startCmd);
+    while (!startCmd.isFinished()) { // waits until end of path before checking for a ball
+    }
+
+    Trajectory choosenTraj;
+    if (magazine.getPC() == 0)
+      choosenTraj = blueTraj;
+    else
+      choosenTraj = redTraj;
+    endCmd = new followTrajectory(drive, choosenTraj);
+    addCommands(endCmd);
   }
 
-  //Called every time the scheduler runs while the command is scheduled.
+  // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
   }
 
-  //Returns true when the command should end.
+  // Returns true when the command should end.
   @Override
   public boolean isFinished() {
     if (startCmd.isFinished())

44e572b9c1f2a9cdee5900576cfb3543aa7858f2
DerekL <derek@adminnet.us>
2021-03-24 13:47:57 -0500

    organize webpage root level

---
+++ FRC-2020/pynetworktables/dist/windows/js/dpl_test.js
@@ -0,0 +1,19 @@
+function DPLConnect() {
+  if (NetworkTables.isRobotConnected() ) 
+  { 
+    console.log("Robot is connected");
+    const keys = NetworkTables.getKeys();
+    for (const k of keys) {
+      if (k.includes("/Commands/")) {
+        console.log(k);
+      }
+    }
+
+    
+  }
+  else {console.log("NO ROBOT - Check that pynetworktables is running."); }
+}
+
+function logMapElements(value, key, map) {
+  console.log(`m[${key}] = ${value}`);
+}

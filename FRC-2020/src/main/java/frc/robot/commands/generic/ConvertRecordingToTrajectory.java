d2ab3a603a154a22c6da2a89dc3ae19c737a01ec
DerekL <derek@adminnet.us>
2021-03-25 15:01:11 -0500

    webpage directory layout org.  WIP traj capture

--- FRC-2020/src/main/java/frc/robot/commands/generic/ConvertRecordingToTrajectory.java
+++ FRC-2020/src/main/java/frc/robot/commands/generic/ConvertRecordingToTrajectory.java
@@ -4,40 +4,65 @@
 
 package frc.robot.commands.generic;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.stream.Stream;
 
-import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.kinematics.DifferentialDriveKinematics;
-import edu.wpi.first.wpilibj.kinematics.DifferentialDriveWheelSpeeds;
 import edu.wpi.first.wpilibj.trajectory.Trajectory;
 import frc.robot.Constants.RobotPhysical;
 import frc.robot.commands.generic.PositionRecorder.RecordLine;
 
 /** Add your docs here. */
-public class ConvertRecordingToTrajectory {
-  DifferentialDriveWheelSpeeds speed;
-  Trajectory traj;
+public class ConvertRecordingToTrajectory { 
+  final static String defaultDirectoryName="recordings";
+  
+  
+  //DifferentialDriveWheelSpeeds speed;
+  Trajectory trajectory;
 
-  Pose2d pose;
-  Trajectory.State state;
   ArrayList<Trajectory.State> states;
-  ArrayList<RecordLine> lines = new ArrayList<>();
-  DifferentialDriveKinematics kinematics = new DifferentialDriveKinematics(RobotPhysical.WheelAxleDistance);
-   
-  public ConvertRecordingToTrajectory() {
+  ArrayList<RecordLine> lines;
+  DifferentialDriveKinematics kinematics;
 
+  String inFileName;
+  Path path;
+ 
+  final String outputDir;
+
+  PrintWriter writer;
+
+  public ConvertRecordingToTrajectory() {
+    this(defaultDirectoryName);
   }
 
+  public ConvertRecordingToTrajectory(String folderName) {
+    kinematics = new DifferentialDriveKinematics(RobotPhysical.WheelAxleDistance);
+    lines = new ArrayList<>();
+    states = new ArrayList<>();
+
+    trajectory = null;
+    inFileName = null;
+    outputDir =  Filesystem.getOperatingDirectory().toString() + "/" +folderName;
+    
+    // make sure we have a directory to save output files in
+    new File(outputDir).mkdirs();
+  }
+
+  // used to calc dt and vel
   double prev_vel = 0;
   double prev_time = 0.0;
 
   public void processRecording() {
     states.clear();
+    prev_vel = 0.0;
+    prev_time = 0.0;
     lines.forEach(r ->
     {
       double time = r.time;
@@ -52,23 +77,58 @@ public class ConvertRecordingToTrajectory {
       prev_vel = vel;
     });
 
+    // to do - decimate and filter the states
+
+    // construct the trajectory
+    trajectory = new Trajectory(states);
   }
-
-
+  /**
+   *  input file to process
+   * 
+   * @param fileName  
+   */
   public void inputFile(String fileName) {
-      Path path = Paths.get(fileName);
-      lines.clear();
+    this.inFileName = fileName;
+    this.path = Paths.get(fileName);
+    lines.clear();
 
-      try 
-      {
+    try {
         Stream<String> stream = Files.lines(path);
         stream.skip(1).forEach(s -> lines.add( RecordLine.fromString(s)) );
         stream.close();
-      } catch (IOException ex) {
+    } catch (IOException ex) {
         System.out.println(ex);
-      }
+    }
+    processRecording();
+  }
+
+  /**
+   * Saves the trajectory file in the output folder
+   */
+  public void saveTrajectory() {
+
+    String root = inFileName.split(".")[0];
+    String output = root + ".json";
+    try { 
+      File f = new File(outputDir, output);
+      f.createNewFile();
+      writer = new PrintWriter(f); // PrintWriter is buffered
+  
+      System.out.println("Recording to: " + f.getAbsolutePath());
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  
 
   }
 
+  /**
+   * 
+   * @return  Trajectory converted from recording
+   */
+  public Trajectory getTrajectory() {
+    return trajectory;
+  }
 
 }

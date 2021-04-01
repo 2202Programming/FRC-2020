e4f66936a0ff02c5d6422d6adff78edac96a99b5
Beast2019 <BeastDriverStn@gmail.com>
2021-04-01 16:41:38 -0500

    matchready power off intake

--- FRC-2020/src/main/java/frc/robot/commands/MatchReadyCmd.java
+++ FRC-2020/src/main/java/frc/robot/commands/MatchReadyCmd.java
@@ -9,6 +9,8 @@ import static frc.robot.subsystems.Magazine_Subsystem.MIN_SOFT_STOP;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.RobotContainer;
+import frc.robot.commands.intake.IntakePower;
+import frc.robot.commands.intake.IntakePower.Power;
 import frc.robot.commands.intake.MagazineAngle;
 import frc.robot.subsystems.Intake_Subsystem;
 import frc.robot.subsystems.Magazine_Subsystem;
@@ -25,6 +27,7 @@ public class MatchReadyCmd extends SequentialCommandGroup {
     //things to ready the bot for starting a match.
     addCommands(
       new MagazineAngle(intake, MIN_SOFT_STOP),     // mag must be lowered
+      new IntakePower(intake, Power.Off, 0.5), 
       new InstantCommand(intake::raiseIntake),      // intake must be up
       new InstantCommand( () -> mag.setPC(3) ),     // most of time we start with 3 PC
       new InstantCommand( positioner::lock)         // lock it so it won't creep up
